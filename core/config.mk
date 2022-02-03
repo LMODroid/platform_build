c3e498f24850eb2b9d559848a5f76792cfdee425
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-02-03 13:57:17 +0330

    build: Call lineage sepolicy repo
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I763eeb79e62b1271c088eca387020952ec351362

--- core/config.mk
+++ core/config.mk
@@ -1170,6 +1170,9 @@ ifneq ($(wildcard device/lmodroid/sepolicy/common/sepolicy.mk),)
 ## last, to avoid accidental resetting by device configs
 $(eval include device/lmodroid/sepolicy/common/sepolicy.mk)
 endif
+ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
